a664ee30aae9e5dd1b1945e8d521bf033e8f0a33
Krishnasai1080 <paidikrishnasai5577@gmail.com>
2024-06-12 00:43:42 -0400

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose");
 const path = require('path')
 
 app.use(express.json()); 
 
 const userRoutes = require("./server/routes/user")
+const userRoutesm = require("./server/routes/user_mongo")
+
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
 
 //CORS middleware
 app.use(function(req, res, next) {
@@ -18,6 +25,7 @@ app.use(express.static(__dirname + "/public"))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/public/index.html")))
 
 app.use('/users', userRoutes)
+app.use('/user_mongo',userRoutesm)
 
 const PORT = process.env.PORT || 3000
 
