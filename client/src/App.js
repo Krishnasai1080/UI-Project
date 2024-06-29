514dd4baa5a7da24c46c4567fbabb28188901238
Krishnasai1080 <paidikrishnasai5577@gmail.com>
2024-06-29 14:25:19 -0400

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './components/pages/Home.js'
 import Navbar from './components/pages/Navbar.js'
 import Register from './components/pages/Register.js'
 import Login from './components/pages/Login.js';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LocalStorageDisplay from './components/pages/Profile.js';
+import Profile from './components/pages/Profile.js';
 function App() {
   return (
     <div className="App">
@@ -14,7 +14,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="Register" element={<Register />} />
             <Route path="Login" element={<Login />} />
-                        <Route path="Profile" element={<LocalStorageDisplay />} />
+                        <Route path="Profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
